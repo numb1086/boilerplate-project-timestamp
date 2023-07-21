762fd283f6a1a3ac4faf18e92625c7561e0e7d22
numb1086 <numb1086@users.noreply.github.com>
2023-07-21 10:30:32 +0800

    Update index.js
    Update format from ES5 to ES6

--- index.js
+++ index.js
@@ -2,33 +2,33 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint... 
-app.get("/api/hello", function(req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api", function(req, res) {
+app.get("/api", (req, res) => {
   let date = new Date();
   res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-app.get("/api/:date?", function(req, res) {
+app.get("/api/:date?", (req, res) => {
   let dateString = req.params.date;
   let unixTime = new Date(dateString).getTime();
   let utcTime = new Date(dateString).toUTCString();
@@ -47,6 +47,6 @@ app.get("/api/:date?", function(req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
